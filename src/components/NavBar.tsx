f140a67bdadff5351a8eb54ebb518aa179da268c
zyadfallatah <zyadfallatah@gmail.com>
2024-08-10 04:30:33 +0300

    Header Added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,14 @@
+import Theme from "./Theme";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="relative flex justify-between px-4 py-8 md:px-20 bg-white dark:bg-dark-color">
+      <h1 className="font-extrabold text-[14px] md:text-2xl dark:text-white">
+        Where in the world?
+      </h1>
+      <Theme />
+    </header>
+  );
 };
 
 export default NavBar;
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -1,7 +1,7 @@
 const Countries = () => {
   return (
     <>
-      <h1 className="text-3xl text-blue-400">Countries Yay</h1>
+      <h1 className="text-3xl text-blue-400"></h1>
     </>
   );
 };
