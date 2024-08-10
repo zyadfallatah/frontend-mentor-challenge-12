4752d61237bfaceaff54b2aeb99ea888fcb9a9c7
zyadfallatah <zyadfallatah@gmail.com>
2024-08-10 06:50:54 +0300

    Edit Header styles for desktop screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import Theme from "./Theme";
 
 const NavBar = () => {
   return (
-    <header className="relative flex justify-between px-4 py-8 md:px-20 bg-white dark:bg-dark-color">
-      <h1 className="font-extrabold text-[14px] md:text-2xl dark:text-white">
-        Where in the world?
-      </h1>
-      <Theme />
+    <header className="px-4 py-8 md:px-20 bg-white dark:bg-dark-color">
+      <div className="flex justify-between max-w-[1440px] mx-auto">
+        <h1 className="font-extrabold text-[14px] md:text-2xl dark:text-white">
+          Where in the world?
+        </h1>
+        <Theme />
+      </div>
     </header>
   );
 };
