ce8d6b10c4e4964b6636d35624fa98e71a6fbf2e
zyadfallatah <zyadfallatah@gmail.com>
2024-08-12 14:18:06 +0300

    White Flags Clarity problem added

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -7,7 +7,7 @@ const Country = ({ country }: { country: CountrySchema }) => {
     <Link to={`/frontend-mentor-challenge-12/${country.name.official}`}>
       <div
         className="bg-white-color dark:bg-dark-color text-dark-color dark:text-white-color min-h-[336px] rounded-b-[5px]
-    cursor-pointer shadow-lg hover:shadow-2xl duration-300"
+    cursor-pointer shadow-lg hover:shadow-2xl duration-100 border border-[#bebebe] dark:border-dark-color"
       >
         <img
           className=" w-full h-[10rem] object-cover rounded-t-[5px]"
