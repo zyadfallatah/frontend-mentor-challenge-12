9eda4034fc3bff721ffd50d390a0a8ec1a93d6ca
zyadfallatah <93343139+zyadfallatah@users.noreply.github.com>
2024-11-23 17:23:46 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend-mentor-challenge-12",
+  base: "/",
   server: {
     port: 3000,
   },
